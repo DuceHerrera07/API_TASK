75f45cc9e3c4a41d0dac3d59a4330089a71c0d0b
DulceHerrera07 <hgdulce267@gmail.com>
2024-06-22 14:47:17 -0600

    Listar todas las tareas GET

--- aplicacion.py
+++ aplicacion.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 #BDD en memoria
 tareas = []
 id_counter = 1
+
+#POST
+@app.route('/tareas', methods=['POST'])
+def crear_tarea():
+    global id_counter
+    data = request.get_json()
+    tarea = {
+        'id': id_counter,
+        'descripcion': data['descripcion'],
+        'fecha_maxima': data['fecha_maxima']
+    }
+    tareas.append(tarea)
+    id_counter += 1
+    return jsonify(tarea), 201
+
+#GET
+@app.route('/tareas', methods=['GET'])
+def listar_tareas():
+    return jsonify(tareas), 200
+
